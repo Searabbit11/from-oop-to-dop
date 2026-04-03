863fcd74f3331693e651808f298a5ec7187566dc
JIWON SHIN <joanne0720@naver.com>
2026-04-03 13:12:56 +0900

    5일차 2

--- Assets/10. Scripts/01. Study ECS/00. Preview/RotationSystem.cs
+++ Assets/10. Scripts/01. Study ECS/00. Preview/RotationSystem.cs
@@ -1,7 +1,25 @@
-﻿namespace _10._Scripts._01._Study_ECS._00._Preview
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Burst;
+
+namespace ECS_Preview
 {
-    public class RotationSystem
+    // ECS에서 System은 로직을 담당합니다.
+    public partial struct RotationSystem : ISystem
     {
-        
+        public void OnUpdate(ref SystemState state)
+        {
+            // 특정 개체를 회전 시키는 로직을 작성해봅시다.
+
+            float deltaTime = SystemAPI.Time.DeltaTime;
+
+            foreach (var (localTransform,speed) in 
+                     SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotationSpeed>>())
+            {
+                localTransform.ValueRW =
+                    localTransform.ValueRO.RotateY(speed.ValueRO.RadiansPerSecond * deltaTime);
+            }
+        }
     }
 }
