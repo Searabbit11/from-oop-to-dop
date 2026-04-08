6eacc73acc89fc68442d73523c05201be1484042
Jay <gabergambler@gmail.com>
2026-04-08 11:47:43 +0900

    Job을 이용한 병렬 계산 처리

--- Assets/10. Scripts/01. Study ECS/01. Basic/ChaserCollisionSystem_Step2.cs
+++ Assets/10. Scripts/01. Study ECS/01. Basic/ChaserCollisionSystem_Step2.cs
@@ -1,9 +1,11 @@
 ﻿using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Rendering;
+using Unity.Transforms;
 using UnityEngine.Rendering.Universal;
 
 namespace ECS_Basic
@@ -33,55 +35,132 @@ namespace ECS_Basic
             // SystemAPI.GetComponentLookup<T>() : T 컴포넌트를 식별할수있는 해시테이블을 요청합니다 
             var colorLookup = SystemAPI.GetComponentLookup<URPMaterialPropertyBaseColor>();
             var targetLookup = SystemAPI.GetComponentLookup<TargetTag>();
+            var transitionLookup = SystemAPI.GetComponentLookup<HitColorTransition>();
+            var transformLookup = SystemAPI.GetComponentLookup<LocalTransform>();
 
             CollisionColorJob colorJob = new CollisionColorJob
             {
                 // colorJob이 임무 수행을위해 참고해야할 자료를 주입해줍니다
                 ColorLookup = colorLookup,
-                TargetLookup = targetLookup, 
+                TargetLookup = targetLookup,
+                TransitionLookup = transitionLookup,
+                LocalTransformLookup = transformLookup,
+                TimeSeed = (uint)(SystemAPI.Time.ElapsedTime * 1000)
             };
 
             // 생성한 Job을 처리할수 있도록 예약을 해둡니다.
-            state.Dependency = colorJob.Schedule(simulation, state.Dependency);
+            JobHandle collisionHandle = colorJob.Schedule(simulation, state.Dependency);
+            
+            // Interpolation Job을 예약해 줍니다
+
+            ColorInterpolationJob interpolationJob = new ColorInterpolationJob()
+            {
+                DeltaTime = SystemAPI.Time.DeltaTime
+            };
+            
+            state.Dependency = interpolationJob.ScheduleParallel(collisionHandle);
         }
     }
     
-    // Target과 충돌했을때 Chaser의 Color를 변경해 봅시다.
-    // 1. Target과 Chaser의 충돌을 검사하고 싶다면
-    // 2. TargetTag와 URPMaterialPropertyBaseColor 두가지의 컴포넌트 데이터
-    // 3. 해당 컴포넌트를 기반으로 만족하는 Entity를 찾아냅니다.
-    // 4. void Execute(TriggerEvent triggerEvent)의 triggerEvent로 전달된 Entity들의 검사를 진행합니다.
+    
+    // 단계별 구현은 Step1 가서 보세요
+    // Step1 상태에서 컬러 보간을 이용해서 색을 빼는 기능을 추가해야 한다고 시나리오를 설정해보겠습니다
     [BurstCompile]
     public struct CollisionColorJob : ITriggerEventsJob
     {
-        // URPMaterialPropertyBaseColor 컴포넌트를 찾기 위한 해시 태이블.
-        // ComponentLookup은 보통 단일개체를 검색할때 사용합니다
         public ComponentLookup<URPMaterialPropertyBaseColor> ColorLookup;
-        public ComponentLookup<TargetTag> TargetLookup; 
-        // 사실 TargetTag가 싱글톤이라서 Entity 형태로 주입해도 됩니다
-        // Ex) public Entity Target;
+        public ComponentLookup<TargetTag> TargetLookup;
         
-        // "triggerEvent가 발생했을때 수행해야할 함수다"라고 생각하신면 됩니다
+        // HitColorTransition을 찾을 lookup
+        public ComponentLookup<HitColorTransition> TransitionLookup;
+        public ComponentLookup<LocalTransform> LocalTransformLookup;
+
+        public float TimeSeed;
+        
+        [BurstCompile]
         public void Execute(TriggerEvent triggerEvent)
         {
             Entity entityA = triggerEvent.EntityA;
             Entity entityB = triggerEvent.EntityB;
             
-            // 내가 생성자를 통해 전달받은 lookUp에 해당 entity가 포함되는지를 검사합니다.
-            // entityA가 TargetTag를 가지고 있는지 TargetLookup에게 물어봅니다 && 
-            // entityB가 URPMaterialPropertyBaseColor를 가지고 있는지 ColorLookup에게 물어봅니다
             if (TargetLookup.HasComponent(entityA) && ColorLookup.HasComponent(entityB))
             {
-                // 충돌이 일어났으면, 색을 변경해줍니다
-                ColorLookup[entityB] = 
-                    new URPMaterialPropertyBaseColor { Value = new float4(1, 0, 0,1) };
+                ApplyCollisionHit(entityB);
+                TeleportTarget(entityA);
             }
-            // 이제부터는 양방향 검사를 해야합니다.
             else if (TargetLookup.HasComponent(entityB) && ColorLookup.HasComponent(entityA))
             {
-                ColorLookup[entityA] = 
-                    new URPMaterialPropertyBaseColor { Value = new float4(1, 0, 0,1) };
+                ApplyCollisionHit(entityA);
+                TeleportTarget(entityB);
             }
         }
+
+        [BurstCompile]
+        private void ApplyCollisionHit(Entity entity)
+        {
+            ColorLookup[entity] = 
+                new URPMaterialPropertyBaseColor { Value = new float4(1, 0, 0,1) };
+            
+            // ColorInterpolationJob을 설정해 줍니다.
+            if (TransitionLookup.HasComponent(entity))
+            {
+                TransitionLookup[entity] = new HitColorTransition
+                {
+                    Timer = 1.0f,
+                    IsActive = true,
+                };
+            }
+        }
+        
+        // Job 안에서 난수를 생성하는것을 짧게 배워봅시다.
+        // 충돌이 일어나면 TargetTag를 가진 개체를 랜덤한 범위로 텔레포트를 시켜 봅시다
+        [BurstCompile]
+        private void TeleportTarget(Entity entity)
+        {
+            uint seed = (uint)(entity.Index + TimeSeed);
+            var random = new Unity.Mathematics.Random(seed); // UnityEngine.Random()을 사용할수가 없습니다 
+            
+            // TargetTag 엔티티의 x, z값만 랜덤으로 바꾸겠습니다
+            float randomX = random.NextFloat(0, 50f);
+            float randomZ = random.NextFloat(0, 50f);
+
+            // TargetTag의 LocalTransform 컴포넌트를 가져 옵니다
+            LocalTransform transform = LocalTransformLookup[entity];
+            transform.Position = new float3(randomX, 0, randomZ);
+            LocalTransformLookup[entity] = transform;
+        }
+        
     }
+
+    [BurstCompile]
+    public partial struct ColorInterpolationJob : IJobEntity
+    {
+        public float DeltaTime; // 생성자를 통해서 주입받을겁니다
+
+        [BurstCompile]
+        public void Execute(ref URPMaterialPropertyBaseColor color, ref HitColorTransition transition)
+        {
+            if (transition.IsActive == false) return;
+
+            transition.Timer -= DeltaTime;
+
+            if (transition.Timer <= 0.0f) // Interpolation이 종료된 경우
+            {
+                transition.IsActive = false;
+                transition.Timer = 0.0f;
+                color.Value = new float4(1, 1, 1, 1);
+            }
+            else // Interpolation을 진행해야 할 경우
+            {
+                // Interpolation 시간은 1초로 설정하겠습니다
+                float t = 1.0f - transition.Timer;
+                float4 startColor = new float4(1, 0, 0, 1); //스타트 컬러는 red부터 시작
+                float4 endColor = new float4(1, 1, 1, 1);
+                
+                color.Value = math.lerp(startColor, endColor, t);
+            }
+        }
+
+    }
+    
 }
